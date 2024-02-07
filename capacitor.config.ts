9f7786b0f3ef605986dfa6b030e2e7052e1b9552
Mateus G. S. de Vasconcelos <94459451+MateusGSV@users.noreply.github.com>
2024-02-07 11:40:05 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'projeto-rpg',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
